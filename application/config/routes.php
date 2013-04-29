a06f2d064ca3f123da179745159e73d6a7e5319e
NATIVEL Pierre-Alexandre <panative@PA-Fixe.(none)>
2013-04-29 07:20:20 +0200

    Add configurations files to the git repository. We should remove these from .gitignore file.

--- application/config/routes.php
+++ application/config/routes.php
@@ -9,7 +9,7 @@
 | and its corresponding controller class/method. The segments in a
 | URL normally follow this pattern:
 |
-|	example.com/class/method/id/
+| 	example.com/class/method/id/
 |
 | In some instances, however, you may want to remap this relationship
 | so that a different class/function is called than the one
@@ -23,23 +23,46 @@
 | RESERVED ROUTES
 | -------------------------------------------------------------------------
 |
-| There area two reserved routes:
+| There is one reserved routes:
 |
 |	$route['default_controller'] = 'welcome';
 |
 | This route indicates which controller class should be loaded if the
 | URI contains no data. In the above example, the "welcome" class
 | would be loaded.
-|
-|	$route['404_override'] = 'errors/page_missing';
-|
-| This route will tell the Router what URI segments to use if those provided
-| in the URL cannot be matched to a valid route.
-|
 */
 
-$route['default_controller'] = "accueil";
-$route['404_override'] = '';
+$route['default_controller'] = "welcome";
+
+# for CI 1.7.2 compatibility, scaffolding is deprecated
+$route['scaffolding_trigger'] = sha1(time());
+
+/*
+| -------------------------------------------------------------------------
+| ROUTES FOR MODULES
+| -------------------------------------------------------------------------
+*/
+
+
+// your URI doesn't have to start with a module name
+$route['site/(:any)/(.*)'] = 'modulerouter/$1/$2';			// only do module routing for this module
+
+// your module URI can be constructed from different URI parts
+$route['(:any)/controller/(.*)'] = 'modulerouter/$1/$2';	// use different segments to route to the module
+
+// you can also route on a per-module bases
+$route['example/(.*)'] = 'modulerouter/example/$1';			// only do module routing for this module
+
+// you can choose to route all your standard application controllers first
+// and everything else to the module router
+$route['(welcome|test)(.*)'] = '$1$2';	// welcome.php and test.php are application controllers
+$route['(.*)'] = 'modulerouter/$1';						// everything is assumed to be a module
+
+/*
+| -------------------------------------------------------------------------
+| CUSTOM ROUTES
+| -------------------------------------------------------------------------
+*/
 
 
 /* End of file routes.php */
