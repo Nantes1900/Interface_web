d7537896411ec691864b84f4ec4b6e1feaa87752
NATIVEL <pierre-alexandre.nativel@mysciencework.com>
2013-05-02 16:30:08 +0200

    Test

--- application/controllers/accueil.php
+++ application/controllers/accueil.php
@@ -12,45 +12,45 @@ class Accueil extends CI_Controller
     {
         parent::__construct();
 
-		//Ce code sera executé charque fois que ce contrôleur sera appelé
-		
-		$this->load->view('header');
-	}
+        //Ce code sera executé charque fois que ce contrôleur sera appelé
+        
+        $this->load->view('header');
+    }
 
-	public function accueil()
-	{
+    public function accueil()
+    {
 
-		$this->load->library('form_validation');
-		$this->load->view('accueil/body');
+        $this->load->library('form_validation');
+        $this->load->view('accueil/body');
 
-		if ( ! $this->session->userdata('username') ) //Si l'utilisateur n'est pas loggé, on affiche le formulaire de connexion
-		{
-			$this->load->view('accueil/login/formulaire_login',array('titre'=>'Connectez-vous :'));
-		}
-		else //Sinon, on affiche les zones restreintes
-		{
+        if ( ! $this->session->userdata('username') ) //Si l'utilisateur n'est pas loggé, on affiche le formulaire de connexion
+        {
+            $this->load->view('accueil/login/formulaire_login',array('titre'=>'Connectez-vous :'));
+        }
+        else //Sinon, on affiche les zones restreintes
+        {
 
-			$data = array('username' => $this->session->userdata('username'));
+            $data = array('username' => $this->session->userdata('username'));
 
-			$this->load->view('accueil/welcome', $data);
-                        
+            $this->load->view('accueil/welcome', $data);
+
                         if ( $this->session->userdata('user_level') == 5)
                         {
                             $this->load->view('accueil/data_center_anchor');
                         }
-                        
-                        
-			$this->load->view('footer');
-		}
 
-	}
 
-	public function signin()
-	{
-		$this->load->library('form_validation');
-		$this->load->view('accueil/signin/formulaire_signin');
-		$this->load->view('footer');
-	}
+            $this->load->view('footer');
+        }
+
+    }
+
+    public function signin()
+    {
+        $this->load->library('form_validation');
+        $this->load->view('accueil/signin/formulaire_signin');
+        $this->load->view('footer');
+    }
 }
 
 /* End of file accueil.php */
