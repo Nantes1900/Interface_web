8cf7814fb6098fb6e6f664790a682d0dfadd384a
paulyves <lucas.paulyves@gmail.com>
2013-05-07 13:25:14 +0200

    admin_panel framework

---
+++ application/controllers/admin_panel/admin_panel.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * admin_panel class
+ * 
+ * Accessible uniquement aux utilisateurs autorisés.
+ * Menu d'administration proposant de gérer les utilisateurs (droits d'accès)
+ * 
+ * @author LUCAS Paul-Yves
+ * 
+ */
+class Admin_panel extends CI_Controller {
+    
+    public function index(){
+        if ( $this->session->userdata('user_level') == 9 ) {
+            $this->load->view('admin_panel/admin_panel');
+        }else if ( !$this->session->userdata('username') ) {
+            $this->load->view('accueil/login/formulaire_login',array('titre'=>'Vous n\'êtes pas connecté. Veuillez vous connecter :'));
+        }
+    }   
+        public function __construct() {
+	
+            parent::__construct();
+
+            //Ce code sera executé charque fois que ce contrôleur sera appelé
+		
+            $this->load->library('form_validation');
+            $this->load->view('header');
+	}    
+}
+
+?>
---
+++ application/views/admin_panel/admin_panel.php
@@ -0,0 +1,7 @@
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" > 
+
+	<h1>Admin panel</h1>
+        
+      
+
+</html>
