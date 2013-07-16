6af4e17335cdb2d190611cadd6d22fbf1cd03611
paulyves <lucas.paulyves@gmail.com>
2013-07-16 11:37:14 +0200

    begin of multi language support

---
+++ system/language/french/form_validation_lang.php
@@ -0,0 +1,29 @@
+<?php
+
+$lang['required']			= "Le champ %s est obligatoire.";
+$lang['isset']				= "Le champ %s doit avoir une valeur.";
+$lang['valid_email']		= "Le champ %s doit contenir une adresse mail valide.";
+$lang['valid_emails']		= "Le champ %s doit contenir des adresses mail valides";
+$lang['valid_url']			= "Le champ %s doit contenir une URL valide.";
+$lang['valid_ip']			= "Le champ %s doit contenir une adresse IP valide.";
+$lang['min_length']			= "Le champ %s doit faire au moins %s caractères de long.";
+$lang['max_length']			= "Le champ %s ne doit pas dépasser %s caractères de long.";
+$lang['exact_length']		= "Le champ %s doit faire exactement %s caractères de long.";
+$lang['alpha']				= "Le champ %s ne doit contenir que des caractères alphabétiques.";
+$lang['alpha_numeric']		= "Le champ %s ne doit contenir que des caractères alpha-numériques.";
+$lang['alpha_dash']			= "Le champ %s ne doit contenir que des caractères alpha-numériques, des underscores et des tirets.";
+$lang['numeric']			= "Le champ %s ne doit contenir que des nombres.";
+$lang['is_numeric']			= "Le champ %s ne doit contenir que des caractères numériques.";
+$lang['integer']			= "Le champ %s doit contenir un entier.";
+$lang['regex_match']		= "Le champ %s n'est pas au bon format.";
+$lang['matches']			= "Le champ %s ne correspond pas au cham %s .";
+$lang['is_unique'] 			= "Le champ %s doit contenir une valeur unique.";
+$lang['is_natural']			= "Le champ %s doit contenir une valeur positive ou nulle.";
+$lang['is_natural_no_zero']	= "Le champ %s doit contenir une valeur strictement positive.";
+$lang['decimal']			= "Le champ %s doit contenir un décimal.";
+$lang['less_than']			= "Le champ %s doit contenir un nombre inférieur à %s.";
+$lang['greater_than']		= "Le champ %s doit contenir un nombre supérieur à %s.";
+
+
+/* End of file form_validation_lang.php */
+/* Location: ./system/language/french/form_validation_lang.php */
